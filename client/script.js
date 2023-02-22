6a087f2d92391aeea7148845f54836015c9dd717
Aquarica <erica_winfrey@yahoo.com>
2023-02-22 01:29:21 -0500

    go

--- client/script.js
+++ client/script.js
@@ -1,11 +1,14 @@
-let inputString = "";
+let inputString = "SUFFER";
 let index = 0;
 let textDelay = 100;
 
 function setup() {
   createCanvas(400, 400);
   textSize(32);
-  getTextFromAPI();
+  const button = createButton('Generate Text');
+  button.mousePressed(getTextFromAPI);
+  userinput = createInput('WHY');
+  userinput.changed(getTextFromAPI);
 }
 
 function draw() {
@@ -34,11 +37,17 @@ function draw() {
 
 let interval;
 async function getTextFromAPI() {
+  console.log("Hello");
+  const userinput = document.querySelector('input').value;
+  
   const response = await fetch("http://localhost:3000/generate", 
-  {method: "post", body: {text:"Little red riding hood went to the forest and "}});
+  {method: "post", body: {text: userinput}});
+  console.log("Hello2");
   const data = await response.json();
   inputString = data.text;
   textDelay = Math.floor(Math.random() * 200) + 50;
+  clearInterval(interval);
+  index = 0;
   interval = setInterval(function() {
     index++;
     if (index > inputString.length) {
