2302509d243eba483eec62702efd2caaf9f241f0
Aquarica <erica_winfrey@yahoo.com>
2023-02-22 23:30:24 -0500

    Meme Generator

--- client/script.js
+++ client/script.js
@@ -1,4 +1,5 @@
-let inputString = "This is the start";
+let inputString = " ";
+let textBoxText = " ";
 let index = 0;
 let textDelay = 100;
 let backgroundImage;
@@ -10,47 +11,65 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   background(backgroundImage);
-  textSize(22);
-  const button = createButton('Generate Response');
+  textSize(18);
+  const button = createButton('Generate Meme');
   button.mousePressed(getTextFromAPI);
-  const inputText = createInput('Enter your text here');
+  const inputText = createInput('');
   inputText.input(function() {
     inputString = inputText.value();
+    
   });
 }
 
 function draw() {
   background(backgroundImage);
-  let displayString = "";
-  let words = inputString.split(" ");
-  let currentLine = "";
-  for (let i = 0; i < words.length; i++) {
-    let word = words[i];
-    if (textWidth(currentLine + word) < width - 50) {
-      currentLine += word + " ";
-    } else {
-      displayString += currentLine + "\n";
-      currentLine = word + " ";
+    // Draw inputString at the bottom half of the canvas
+    // textAlign(LEFT);
+    // let inputStringY = height - textDescent() - 10; // 10 is a padding
+    // text(inputString, width/2, inputStringY);
+    
+    // Draw textBoxText at the top half of the canvas
+    // textAlign(LEFT);
+    textFont('Helvetica');
+    let textBoxTextY = textAscent() + 10; // 10 is a padding
+    text(textBoxText, 170, textBoxTextY+40);
+    
+    //Split and draw long strings as multiple lines
+    let displayString = "";
+    let words = inputString.split(" ");
+    let currentLine = "";
+    textFont('Brush Script MT');
+    
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+      if (textWidth(currentLine + word) < width - 100) {
+        currentLine += word + " ";
+      } else {
+        displayString += currentLine + "\n";
+        currentLine = word + " ";
+      }
+    }
+    displayString += currentLine;
+    let lines = displayString.split("\n");
+    let lineHeight = textAscent() + textDescent();
+    for (let i = 0; i < lines.length; i++) {
+      let lineText = lines[i];
+      let y = (height + lineHeight) / 3 + lineHeight * i;
+      //textAlign(LEFT, CENTER);
+      text(lineText, 170, y+130);
     }
   }
-  displayString += currentLine;
-  let lines = displayString.split("\n");
-  let lineHeight = textAscent() + textDescent();
-  for (let i = 0; i < lines.length; i++) {
-    let lineText = lines[i];
-    let y = (height + lineHeight) / 2 + lineHeight * i;
-    text(lineText, 50, y);
-  }
-}
 
 let interval;
 async function getTextFromAPI() {
   console.log("ello" + inputString);
+  textBoxText = inputString;
   const response = await fetch("http://localhost:3000/generate", 
   {method: "post", body:{text: inputString}});
   const data = await response.json();
   console.log(data);
   inputString = data.text;
+  
   textDelay = Math.floor(Math.random() * 200) + 50;
   clearInterval(interval);
   index = 0;
